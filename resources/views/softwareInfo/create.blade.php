0cabecec92ceac5d6bd28f679c9ce19791bf445e
tuhindewan <tuhinsshadow@gmail.com>
2021-02-23 11:22:01 +0600

    ADP-341: Update software information - UI integration 	- Create page code and design upgrade

--- resources/views/softwareInfo/create.blade.php
+++ resources/views/softwareInfo/create.blade.php
@@ -1,220 +1,87 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Validation Form</title>
+@extends('layouts.master')
+@section('title', 'Create New Software')
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
-  <!-- Select2 -->
-  <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/css/bootstrap-select.css" />
-</head>
-<body class="hold-transition sidebar-mini">
-<div class="wrapper">
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="../../index3.html" class="nav-link">Home</a>
-      </li>
-    </ul>
-
-    <!-- SEARCH FORM -->
-    <form class="form-inline ml-3">
-      <div class="input-group input-group-sm">
-        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-navbar" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-      </div>
-    </form>
-
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
-      <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-              Dashboard
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="../widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-building"></i>
-              <p>
-                Office
-              </p>
-            </a>
-          </li>   
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Software Information</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Office</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <!-- left column -->
-          <div class="col-md-12">
-            <!-- jquery validation -->
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Office Name</h3>
+@section('content')
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <div class="row">
+              <div class="col-md-10">
+                <h3 class="card-title">Create New Software</h3>
               </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              <form id="quickForm" action="{{ route('software-infos.store') }}" method="POST">
-                @csrf
-                <div class="card-body">
-                  <div class="form-group">
-                    <label for="name">Office</label>
-                    <select class="selectpicker required form-control"  data-live-search="true" name="office_id">
-                      <option style="display: none"></option>
-                      @foreach($offices as $office)
-                       <option value={{$office->id}}>{{$office->name}}</option>
-                      @endforeach
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="name">Sub-office</label>
-                    <select class="selectpicker required form-control" id="cat"  data-live-search="true" name="sub_office_id">
-                      <option></option>
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="name" name="name" class="form-control" id="name" placeholder="Enter software name">
-                  </div>
-                  <div class="form-group">
-                    <label for="url">URL</label>
-                    <input type="url" name="url" class="form-control" id="url" placeholder="Enter URL">
-                  </div>
-                  <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="name" name="username" class="form-control" id="username" placeholder="Enter username">
-                  </div>
-                  <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="name" name="password" class="form-control" id="password" placeholder="Enter password">
-                  </div>
-                </div>
-                <!-- /.card-body -->
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-              </form>
-            </div>
-            <!-- /.card -->
-            </div>
-          <!--/.col (left) -->
-          <!-- right column -->
-          <div class="col-md-6">
-
+              <div class="col-md-2 float-right">
+                <a href="{{ route('software-infos.index') }}" type="button" class="btn btn-info btn-sm btn-block btn-round">
+                  <i class="fa fa-list"></i> List
+                </a>
+              </div>
+            </div> 
           </div>
-          <!--/.col (right) -->
+          <form id="quickForm" action="{{ route('software-infos.store') }}" method="POST">
+            @csrf
+            <div class="card-body">
+              <div class="form-group">
+                <label for="name">Office</label>
+                <select class="selectpicker required form-control"  data-live-search="true" name="office_id">
+                  <option style="display: none"></option>
+                  @foreach($offices as $office)
+                    <option value="{{$office->id}}">{{$office->name}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="name">Sub-office</label>
+                <select class="selectpicker required form-control" id="cat"  data-live-search="true" name="sub_office_id">
+                  <option></option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="name">Name</label>
+                <input type="name" name="name" class="form-control" id="name" placeholder="Enter software name">
+              </div>
+              <div class="form-group">
+                <label for="url">URL</label>
+                <input type="url" name="url" class="form-control" id="url" placeholder="Enter URL">
+              </div>
+              <div class="form-group">
+                <label for="username">Username</label>
+                <input type="name" name="username" class="form-control" id="username" placeholder="Enter username">
+              </div>
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="name" name="password" class="form-control" id="password" placeholder="Enter password">
+              </div>
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">
+              <button type="submit" class="btn btn-primary">
+                  <i class="fa fa-check"></i> Save
+              </button>
+              <a class="btn btn-warning mr-1" role="button" href="{{ route('software-infos.index') }}">
+                  <i class="fa fa-times"></i> Cancel
+              </a>
+            </div>
+          </form>
         </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.1.0-rc
+        <!-- /.card -->
+        </div>
+      <!--/.col (left) -->
+      <!-- right column -->
+      <div class="col-md-6">
+
+      </div>
+      <!--/.col (right) -->
     </div>
-    <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
+    <!-- /.row -->
+  </div><!-- /.container-fluid -->
+</section>
+@endsection
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- jquery-validation -->
-<script src="{{ asset('plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-<script src="{{ asset('plugins/jquery-validation/additional-methods.min.js') }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('dist/js/demo.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
-<!-- Page specific script -->
+@push('page-js')
 <script>
 $(function () {
-
   $.validator.setDefaults({
     submitHandler: function () {
       alert( "Form successful submitted!" );
@@ -246,7 +113,6 @@ $(function () {
     }
   });
 });
-
 jQuery(document).ready(function ()
     {
         jQuery('select[name="office_id"]').on('change',function(){
@@ -275,5 +141,4 @@ jQuery(document).ready(function ()
         });
     });
 </script>
-</body>
-</html>
+@endpush
